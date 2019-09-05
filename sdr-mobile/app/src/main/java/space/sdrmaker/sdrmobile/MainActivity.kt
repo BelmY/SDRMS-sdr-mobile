aea4f00fa8c45a54e4872d53ec8a378c11f87a47
Jacek Chmielewski <jchmielewski@teonite.com>
2019-09-05 12:42:54 +0200

    (#11) Implement HackRF RX.
    TODO: UI state changes, RX stop.

--- sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/MainActivity.kt
+++ sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/MainActivity.kt
@@ -7,6 +7,9 @@ import android.widget.Button
 import com.mantz_it.hackrf_android.HackrfCallbackInterface
 import kotlinx.android.synthetic.main.activity_main.*
 import com.mantz_it.hackrf_android.Hackrf
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+import kotlin.reflect.typeOf
 
 
 class MainActivity : AppCompatActivity(), HackrfCallbackInterface {
@@ -37,7 +40,8 @@ class MainActivity : AppCompatActivity(), HackrfCallbackInterface {
         }
         startButton = findViewById(R.id.startButton)
         startButton.setOnClickListener {
-            tvOutput.append("RX Start pressed\n")
+            tvOutput.append("RX Start\n")
+            thread {this.receiveThread()}
         }
 
         tvOutput.append("Ready...\n")
@@ -63,21 +67,83 @@ class MainActivity : AppCompatActivity(), HackrfCallbackInterface {
     }
 
     private fun receiveThread() {
-        printOnScreen("Setting sample rate to $samplingRate sps")
+        printOnScreen("Setting sample rate to $samplingRate sps ... ")
         hackrf.setSampleRate(samplingRate, 1)
-        printOnScreen("ok.\nSetting center requency to $centerFreq Hz")
+        printOnScreen("ok.\nSetting center requency to $centerFreq Hz ... ")
         hackrf.setFrequency(centerFreq)
-        printOnScreen("ok.\nSetting baseband filter bandwidth to $bandwidth Hz")
+        printOnScreen("ok.\nSetting baseband filter bandwidth to $bandwidth Hz ... ")
         hackrf.setBasebandFilterBandwidth(bandwidth)
-        printOnScreen("ok.\nSetting RX VGA Gain to $vgaGain")
+        printOnScreen("ok.\nSetting RX VGA Gain to $vgaGain ... ")
         hackrf.setRxVGAGain(vgaGain);
-        printOnScreen("ok.\nSetting LNA Gain to $lnaGain")
+        printOnScreen("ok.\nSetting LNA Gain to $lnaGain ... ")
         hackrf.setRxLNAGain(lnaGain)
-        printOnScreen("ok.\nSetting Amplifier to $amp")
+        printOnScreen("ok.\nSetting Amplifier to $amp ... ")
         hackrf.setAmp(amp)
-        printOnScreen("ok.\nSetting Antenna Power to $antennaPower")
+        printOnScreen("ok.\nSetting Antenna Power to $antennaPower ... ")
         hackrf.setAntennaPower(antennaPower)
         printOnScreen("ok.\n\n")
+
+        printOnScreen("Start Receiving... \n")
+        val queue = hackrf.startRX()
+
+        // Run until user hits the 'Stop' button
+        var i = 0
+        var lastTransceiverPacketCounter = 0L
+        var lastTransceivingTime = 0L
+        while (true) {
+            i++    // only for statistics
+
+            // Grab one packet from the top of the queue. Will block if queue is
+            // empty and timeout after one second if the queue stays empty.
+            val receivedBytes = queue.poll(1000, TimeUnit.MILLISECONDS)
+
+            /*  HERE should be the DSP portion of the app. The receivedBytes
+             *  variable now contains a byte array of size hackrf.getPacketSize().
+             *  This is currently set to 16KB, but may change in the future.
+             *  The bytes are interleaved, 8-bit, signed IQ samples (in-phase
+             *  component first, followed by the quadrature component):
+             *
+             *  [--------- first sample ----------]   [-------- second sample --------]
+             *         I                  Q                  I                Q ...
+             *  receivedBytes[0]   receivedBytes[1]   receivedBytes[2]       ...
+             *
+             *  Note: Make sure you read from the queue fast enough, because if it runs
+             *  full, the hackrf_android library will abort receiving and go back to
+             *  OFF mode.
+             */
+
+            //
+            if (receivedBytes != null) {
+                printOnScreen(".")
+
+                // IMPORTANT: After we used the receivedBytes buffer and don't need it any more,
+                // we should return it to the buffer pool of the hackrf! This will save a lot of
+                // allocation time and the garbage collector won't go off every second.
+                hackrf.returnBufferToBufferPool(receivedBytes);
+            } else {
+                printOnScreen("Error: buffer underrun, RX stopped\n")
+                // TODO: update UI state
+                break
+            }
+
+            // print statistics
+            if (i.rem(100) == 0) {
+                val bytes =
+                    (hackrf.transceiverPacketCounter - lastTransceiverPacketCounter) * hackrf.packetSize
+                val time = (hackrf.transceivingTime - lastTransceivingTime) / 1000.0
+                printOnScreen(
+                    String.format(
+                        "\nCurrent Transfer Rate: %4.1f MB/s\n",
+                        bytes / time / 1000000.0
+                    )
+                )
+                printOnScreen("I: ${receivedBytes[0].toInt()} Q: ${receivedBytes[1].toInt()}")
+                printOnScreen("I: ${receivedBytes[2].toInt()} Q: ${receivedBytes[3].toInt()}\n")
+                printOnScreen("${receivedBytes[0].javaClass.canonicalName}\n")
+                lastTransceiverPacketCounter = hackrf.transceiverPacketCounter
+                lastTransceivingTime = hackrf.transceivingTime
+            }
+        }
     }
 
     private fun printOnScreen(msg: String) {
