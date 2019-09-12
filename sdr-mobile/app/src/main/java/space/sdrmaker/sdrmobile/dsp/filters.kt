ec58ac9bc427c84c984bce484a709f1f46a47486
Jacek Chmielewski <jchmielewski@teonite.com>
2019-09-12 10:16:40 +0200

    (#14) Implement complex downsampler, upsampler, decimator, interpolator, fir filter and resampler.

--- sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/dsp/filters.kt
+++ sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/dsp/filters.kt
@@ -1,14 +1,11 @@
 package space.sdrmaker.sdrmobile.dsp
 
 
-class FIRFilter(private val input: Iterator<Float>, private val coefs: FloatArray) : Iterator<Float> {
+class FIRFilter(private val input: Iterator<Float>, private val coefs: FloatArray) :
+    Iterator<Float> {
     private val length: Int = coefs.size
-    private val delayLine: FloatArray
+    private val delayLine = FloatArray(length)
     private var count = 0
-
-    init {
-        delayLine = FloatArray(length)
-    }
 
     override fun next(): Float {
         val inputSample = input.next()
@@ -24,4 +21,33 @@ class FIRFilter(private val input: Iterator<Float>, private val coefs: FloatArra
     }
 
     override fun hasNext(): Boolean = input.hasNext()
+}
+
+class ComplexFIRFilter(
+    private val input: Iterator<Pair<Float, Float>>,
+    private val coefs: FloatArray
+) : Iterator<Pair<Float, Float>> {
+    private val length: Int = coefs.size
+    private val reDelayLine = FloatArray(length)
+    private val imDelayLine = FloatArray(length)
+    private var count = 0
+
+    override fun next(): Pair<Float, Float> {
+        val inputSample = input.next()
+        reDelayLine[count] = inputSample.first
+        imDelayLine[count] = inputSample.second
+        var reResult = 0f
+        var imResult = 0f
+        var index = count
+        for (i in 0 until length) {
+            reResult += coefs[i] * reDelayLine[index]
+            imResult += coefs[i] * imDelayLine[index]
+            index--
+            if (index < 0) index = length - 1
+        }
+        if (++count >= length) count = 0
+        return Pair(reResult, imResult)
+    }
+
+    override fun hasNext() = input.hasNext()
 }
--- sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/dsp/resamplers.kt
+++ sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/dsp/resamplers.kt
@@ -1,39 +1,55 @@
 package space.sdrmaker.sdrmobile.dsp
 
-val FIR_COEFFS = floatArrayOf(-7.383784e-03f,
--3.183046e-03f,  2.255039e-03f,   7.461166e-03f,   1.091908e-02f,
-1.149109e-02f,   8.769802e-03f,   3.252932e-03f,   -3.720606e-03f,
--1.027446e-02f,  -1.447403e-02f,  -1.486427e-02f,  -1.092423e-02f,
--3.307958e-03f,  6.212477e-03f,   1.511364e-02f,   2.072873e-02f,
-2.096037e-02f,   1.492345e-02f,   3.347624e-03f,   -1.138407e-02f,
--2.560252e-02f,  -3.507114e-02f,  -3.591225e-02f,  -2.553830e-02f,
--3.371569e-03f,  2.882645e-02f,   6.711368e-02f,   1.060042e-01f,
-1.394643e-01f,   1.620650e-01f,   1.700462e-01f,   1.620650e-01f,
-1.394643e-01f,   1.060042e-01f,   6.711368e-02f,   2.882645e-02f,
--3.371569e-03f,  -2.553830e-02f,  -3.591225e-02f,  -3.507114e-02f,
--2.560252e-02f,  -1.138407e-02f,  3.347624e-03f,   1.492345e-02f,
-2.096037e-02f,   2.072873e-02f,   1.511364e-02f,   6.212477e-03f,
--3.307958e-03f,  -1.092423e-02f,  -1.486427e-02f,  -1.447403e-02f,
--1.027446e-02f,  -3.720606e-03f,  3.252932e-03f,   8.769802e-03f,
-1.149109e-02f,   1.091908e-02f,   7.461166e-03f,   2.255039e-03f,
--3.183046e-03f,  -7.383784e-03f)
+val FIR_COEFFS = floatArrayOf(
+    -7.383784e-03f,
+    -3.183046e-03f, 2.255039e-03f, 7.461166e-03f, 1.091908e-02f,
+    1.149109e-02f, 8.769802e-03f, 3.252932e-03f, -3.720606e-03f,
+    -1.027446e-02f, -1.447403e-02f, -1.486427e-02f, -1.092423e-02f,
+    -3.307958e-03f, 6.212477e-03f, 1.511364e-02f, 2.072873e-02f,
+    2.096037e-02f, 1.492345e-02f, 3.347624e-03f, -1.138407e-02f,
+    -2.560252e-02f, -3.507114e-02f, -3.591225e-02f, -2.553830e-02f,
+    -3.371569e-03f, 2.882645e-02f, 6.711368e-02f, 1.060042e-01f,
+    1.394643e-01f, 1.620650e-01f, 1.700462e-01f, 1.620650e-01f,
+    1.394643e-01f, 1.060042e-01f, 6.711368e-02f, 2.882645e-02f,
+    -3.371569e-03f, -2.553830e-02f, -3.591225e-02f, -3.507114e-02f,
+    -2.560252e-02f, -1.138407e-02f, 3.347624e-03f, 1.492345e-02f,
+    2.096037e-02f, 2.072873e-02f, 1.511364e-02f, 6.212477e-03f,
+    -3.307958e-03f, -1.092423e-02f, -1.486427e-02f, -1.447403e-02f,
+    -1.027446e-02f, -3.720606e-03f, 3.252932e-03f, 8.769802e-03f,
+    1.149109e-02f, 1.091908e-02f, 7.461166e-03f, 2.255039e-03f,
+    -3.183046e-03f, -7.383784e-03f
+)
 
-class Upsampler (private val input: Iterator<Float>, private val factor: Int) : Iterator<Float> {
-    var state = -1
+class Upsampler(private val input: Iterator<Float>, private val factor: Int) : Iterator<Float> {
+    private var state = -1
 
     override fun next(): Float {
         state++
         return if (state.rem(factor) == 0) {
             state = 0
             input.next()
-        }
-        else 0F
+        } else 0F
     }
 
-    override fun hasNext(): Boolean = input.hasNext()
+    override fun hasNext() = input.hasNext()
 }
 
-class Downsampler (private val input: Iterator<Float>, private val factor: Int) : Iterator<Float> {
+class ComplexUpsampler(private val input: Iterator<Pair<Float, Float>>, private val factor: Int) :
+    Iterator<Pair<Float, Float>> {
+    private var state = -1
+
+    override fun next(): Pair<Float, Float> {
+        state++
+        return if (state.rem(factor) == 0) {
+            state = 0
+            input.next()
+        } else Pair(0F, 0F)
+    }
+
+    override fun hasNext() = input.hasNext()
+}
+
+class Downsampler(private val input: Iterator<Float>, private val factor: Int) : Iterator<Float> {
 
     override fun next(): Float {
         val out = input.next()
@@ -43,7 +59,21 @@ class Downsampler (private val input: Iterator<Float>, private val factor: Int) 
         return out
     }
 
-    override fun hasNext(): Boolean = input.hasNext()
+    override fun hasNext() = input.hasNext()
+}
+
+class ComplexDownsampler(private val input: Iterator<Pair<Float, Float>>, private val factor: Int) :
+    Iterator<Pair<Float, Float>> {
+
+    override fun next(): Pair<Float, Float> {
+        val out = input.next()
+        for (i in 1 until factor)
+            input.next()
+
+        return out
+    }
+
+    override fun hasNext() = input.hasNext()
 }
 
 class Decimator(input: Iterator<Float>, factor: Int) : Iterator<Float> {
@@ -51,12 +81,26 @@ class Decimator(input: Iterator<Float>, factor: Int) : Iterator<Float> {
     private val filter = FIRFilter(input, calculateFilterCoeffs())
     private val downsampler = Downsampler(filter, factor)
 
-
     override fun hasNext() = downsampler.hasNext()
 
     override fun next(): Float {
         return downsampler.next()
     }
+
+    private fun calculateFilterCoeffs() = FIR_COEFFS
+}
+
+class ComplexDecimator(input: Iterator<Pair<Float, Float>>, factor: Int) :
+    Iterator<Pair<Float, Float>> {
+
+    private val filter = ComplexFIRFilter(input, calculateFilterCoeffs())
+    private val downsampler = ComplexDownsampler(filter, factor)
+
+    override fun next(): Pair<Float, Float> {
+        return downsampler.next()
+    }
+
+    override fun hasNext() = downsampler.hasNext()
 
     private fun calculateFilterCoeffs() = FIR_COEFFS
 }
@@ -75,9 +119,38 @@ class Interpolator(input: Iterator<Float>, factor: Int) : Iterator<Float> {
     private fun calculateFilterCoeffs() = FIR_COEFFS
 }
 
+class ComplexInterpolator(input: Iterator<Pair<Float, Float>>, factor: Int) :
+    Iterator<Pair<Float, Float>> {
+
+    private val upsampler = ComplexUpsampler(input, factor)
+    private val filter = ComplexFIRFilter(upsampler, calculateFilterCoeffs())
+
+    override fun hasNext() = filter.hasNext()
+
+    override fun next(): Pair<Float, Float> {
+        return filter.next()
+    }
+
+    private fun calculateFilterCoeffs() = FIR_COEFFS
+}
+
+
 class Resampler(input: Iterator<Float>, interpolation: Int, decimation: Int) : Iterator<Float> {
+
     private val interpolator = Interpolator(input, interpolation)
     private val decimator = Decimator(interpolator, decimation)
+
+    override fun hasNext() = decimator.hasNext()
+
+    override fun next() = decimator.next()
+
+}
+
+class ComplexResampler(input: Iterator<Pair<Float, Float>>, interpolation: Int, decimation: Int) :
+    Iterator<Pair<Float, Float>> {
+
+    private val interpolator = ComplexInterpolator(input, interpolation)
+    private val decimator = ComplexDecimator(interpolator, decimation)
 
     override fun hasNext() = decimator.hasNext()
 
