f3a09426d467f9758f904a2c40e269ee19df20b3
Jacek Chmielewski <jchmielewski@teonite.com>
2019-10-15 10:09:22 +0200

    (#38) Initial Hilbert transform implementation.

--- sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/dsp/math.kt
+++ sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/dsp/math.kt
@@ -1,5 +1,9 @@
 package space.sdrmaker.sdrmobile.dsp
 
+import org.jtransforms.fft.FloatFFT_1D
+import kotlin.math.ceil
+import kotlin.math.floor
+
 class Multiply(
     private val input1: Iterator<FloatArray>,
     private val input2: Iterator<FloatArray>,
@@ -25,4 +29,37 @@ class Multiply(
         return result
     }
 
+}
+
+class HilbertTransform(
+    private val input: Iterator<FloatArray>
+) : Iterator<FloatArray> {
+
+    private var length = -1
+    private lateinit var fft: FloatFFT_1D
+
+    override fun next(): FloatArray {
+        val nextArray = input.next()
+        if(length < 0) {
+            length = nextArray.size
+            fft = FloatFFT_1D(length.toLong())
+        }
+        val dataFFT = FloatArray(length * 2)
+        nextArray.copyInto(dataFFT)
+        fft.realForwardFull(dataFFT)
+        val nyquist = floor(length / 2 + .5).toInt()
+        val hilbert = FloatArray(ceil(nextArray.size.toFloat() / 2).toInt()) { index -> if(index < nyquist) 2f else 0f}
+        hilbert[0] = 1f
+        hilbert[nyquist] = 1f
+
+        for(i in 0 until length) {
+            dataFFT[2 * i] *= hilbert[i]
+            dataFFT[2 * i + 1] *= hilbert[i]
+        }
+
+        fft.complexInverse(dataFFT, false)
+        return dataFFT
+    }
+
+    override fun hasNext() = input.hasNext()
 }
