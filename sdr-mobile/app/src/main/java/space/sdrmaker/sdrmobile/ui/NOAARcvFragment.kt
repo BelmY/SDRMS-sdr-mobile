db4f65cf8bf318ac4e28bdec75366df802b6690f
Jacek Chmielewski <jchmielewski@teonite.com>
2019-12-28 18:23:45 +0100

    Check if GUI was already initialized in onCreateView.

--- sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/ui/NOAARcvFragment.kt
+++ sdr-mobile/app/src/main/java/space/sdrmaker/sdrmobile/ui/NOAARcvFragment.kt
@@ -65,44 +65,42 @@ class NOAARcvFragment : Fragment(), HackrfCallbackInterface {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        root = inflater.inflate(R.layout.fragment_noaarcv, container, false)
+        if(!this::uiState.isInitialized) {
+            // setup UI
+            root = inflater.inflate(R.layout.fragment_noaarcv, container, false)
+            recButton = root.recButton
+            rxButton = root.rxButton
+            fftPlot = root.fftPlot as FFTView
+            noaaSelector = root.noaaSelector
+            val spinnerArrayAdapter = ArrayAdapter<String>(
+                context!!, android.R.layout.simple_spinner_item,
+                arrayOf("NOAA 15", "NOAA 18", "NOAA 19")
+            )
+            spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            noaaSelector.adapter = spinnerArrayAdapter
+            noaaSelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
 
-        // setup UI
-        recButton = root.recButton
-        rxButton = root.rxButton
-        rxButton.setOnClickListener {
-            startRX()
-        }
-        fftPlot = root.fftPlot as FFTView
-        noaaSelector = root.noaaSelector
-        val spinnerArrayAdapter = ArrayAdapter<String>(
-            context!!, android.R.layout.simple_spinner_item,
-            arrayOf("NOAA 15", "NOAA 18", "NOAA 19")
-        )
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        noaaSelector.adapter = spinnerArrayAdapter
-        noaaSelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                when (position) {
-                    0 -> setNOAA(15)
-                    1 -> setNOAA(18)
-                    2 -> setNOAA(19)
-                    else -> setNOAA(15)
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    when (position) {
+                        0 -> setNOAA(15)
+                        1 -> setNOAA(18)
+                        2 -> setNOAA(19)
+                        else -> setNOAA(15)
+                    }
                 }
             }
+            tvOutput = root.findViewById<Button>(R.id.tvOutput)
+            setUIState(UIState.STARTED)
+            tvOutput.movementMethod = ScrollingMovementMethod()
+            tvOutput.append("Ready...\n")
         }
-        tvOutput = root.findViewById<Button>(R.id.tvOutput)
-        setUIState(UIState.STARTED)
-        tvOutput.movementMethod = ScrollingMovementMethod()
-        tvOutput.append("Ready...\n")
 
         return root
     }
